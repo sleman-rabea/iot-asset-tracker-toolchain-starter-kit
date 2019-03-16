453d385c567d0635e33bbaa1d8f2b9146966c89c
a7md-s <enga7md1992@gmail.com>
2019-03-16 02:23:32 +0200

    Update files

--- contracts/iot-asset-tracker-toolchain-starter-kit/lib/logic.js
+++ contracts/iot-asset-tracker-toolchain-starter-kit/lib/logic.js
@@ -11,14 +11,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-"use strict";
+'use strict';
 /**
  * A shipment has been received by an importer
  * @param {org.acme.shipping.perishable.ShipmentReceived} shipmentReceived - the ShipmentReceived transaction
  * @transaction
  */
-
-
 function payOut(shipmentReceived) {
 
     var contract = shipmentReceived.shipment.contract;
@@ -162,7 +160,7 @@ function AccelReading(AccelReading) {
         AccelerationEvent.accel_x = AccelReading.accel_x;
         AccelerationEvent.accel_y = AccelReading.accel_y;
         AccelerationEvent.accel_z = AccelReading.accel_z;
-	AccelerationEvent.latitude = AccelReading.latitude;
+	    AccelerationEvent.latitude = AccelReading.latitude;
         AccelerationEvent.longitude = AccelReading.longitude;
         AccelerationEvent.readingTime = AccelReading.readingTime;
         AccelerationEvent.message = 'Acceleration threshold violated! Emitting AccelerationEvent for shipment: ' + shipment.$identifier;
@@ -184,7 +182,7 @@ function AccelReading(AccelReading) {
 function gpsReading(gpsReading) {
 
     var factory = getFactory();
-    var NS = "org.acme.shipping.perishable";
+    var NS = 'org.acme.shipping.perishable';
     var shipment = gpsReading.shipment;
     var PORT_OF_NEW_YORK = '/LAT:40.6840N/LONG:74.0062W';
 
@@ -197,7 +195,7 @@ function gpsReading(gpsReading) {
     var latLong = '/LAT:' + gpsReading.latitude + gpsReading.latitudeDir + '/LONG:' +
     gpsReading.longitude + gpsReading.longitudeDir;
 
-    if (latLong == PORT_OF_NEW_YORK) {
+    if (latLong === PORT_OF_NEW_YORK) {
         var shipmentInPortEvent = factory.newEvent(NS, 'ShipmentInPortEvent');
         shipmentInPortEvent.shipment = shipment;
         var message = 'Shipment has reached the destination port of ' + PORT_OF_NEW_YORK;
